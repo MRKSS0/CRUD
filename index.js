7f58dd3d512f87af4349e8ad6a5560d0ea597b9c
MRKSS0 <marco.saccuta02@gmail.com>
2023-04-24 15:48:59 +0200

    U

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+/**
+ * IMPORTING MODULES
+ */
 const express = require('express');
-const app = express();
 const path = require('path');
+
+/**
+ * FOR process.env
+ */
+require('dotenv').config();
+
+/**
+ * IMPORTING ROUTES
+ */
 const server = require('./database/db.config');
 const homepage = require('./routes/homepage');
 const deleteP = require('./routes/delete.js');
 const editor = require('./routes/editor.js');
 const add = require('./routes/add.js');
+
+const app = express();
 app.use(express.static(path.join(__dirname, './public')));
 app.use('/css', express.static(path.join(__dirname, './node_modules/bootstrap/dist/css')));
 
-require('dotenv').config();
 
 const port = process.env.PORT;
 
